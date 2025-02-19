b05f728796a3b2971364f2a49d416695b7d123b6
vishalawaken <tsinghvishall@gmail.com>
2025-02-19 20:10:54 +0530

    new

--- src/api/logo/controllers/logo.ts
+++ src/api/logo/controllers/logo.ts
@@ -5,6 +5,4 @@
 import { factories } from '@strapi/strapi'
 import type { Strapi } from '@strapi/strapi'
 
-export default factories.createCoreController('api::logo.logo', ({ strapi }) => ({
-  // Your custom controller methods can go here
-}));
+export default factories.createCoreController('api::logo.logo');
